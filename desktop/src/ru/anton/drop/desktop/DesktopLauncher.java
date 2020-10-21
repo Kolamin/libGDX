527ee751f66eae9d23194b48916a1c66b4825d56
Kolamin <gofranich@gmail.com>
2020-10-21 18:05:17 +0600

    Lesson3

---
+++ desktop/src/ru/anton/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.anton.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.anton.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
